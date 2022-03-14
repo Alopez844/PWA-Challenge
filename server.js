69ad56294fb6c640582ea85310b76d0087a95bc9
unknown <lopez1432.al@gmail.com>
2022-03-14 15:46:43 -0700

    eroor deplying

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const compression = require("compression");
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
+app.use(logger("dev"));
 
-
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
