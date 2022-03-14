1dc00a69fea846d96a24ee6625369bc81a06a8e2
unknown <lopez1432.al@gmail.com>
2022-03-14 15:37:08 -0700

    deployment fix

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
-app.use(logger("dev"));
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
